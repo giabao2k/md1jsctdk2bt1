80c08b72ed29c3782e4a582a965f1fe735a55b65
giabao2k <ngonguyengiabao2000@gmail.com>
2022-05-11 01:01:58 +0700

    bo comment trong style

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ anh = document.getElementById('quabong');
 function init(){
     anh = document.getElementById('quabong'); 
     anh.style.position = 'relative'; //lay minh lam goc toa do khong phu thuoc vao doi tuong nao o ngoai ca
-    // anh.style.right = '0px'; // style chỉnh khoảng cách từ cạnh mh đến quả bóng
+    anh.style.right = '0px'; // style chỉnh khoảng cách từ cạnh mh đến quả bóng
 }
 function dichuyen() {
     anh.style.right = parseInt(anh.style.right) + 10 + 'px' ;
